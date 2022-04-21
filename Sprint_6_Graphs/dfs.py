3e83e4af85a7525fdc794232c64cf46b049c9d3b
tolmachina <tolmachina@gmail.com>
2022-04-21 15:09:11 +0100

    dfs class

---
+++ Sprint_6_Graphs/dfs.py
@@ -0,0 +1,15 @@
+class Graph():
+    def __init__(self):
+        self.graph = []
+    
+    def add(self, node):
+        pass
+    
+    def dfs(self, value):
+        pass
+
+    def bfs(self, value):
+        pass
+
+    def print(self):
+        pass
